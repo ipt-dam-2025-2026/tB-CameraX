dacab081eaa91e40303b2675bf86c703976fbf5e
jcnpereira <casimiro@ipt.pt>
2025-10-31 20:20:40 +0000

    Start using the 'binding view'

--- camerax/app/src/main/java/pt/ipt/dam2025/camerax/MainActivity.kt
+++ camerax/app/src/main/java/pt/ipt/dam2025/camerax/MainActivity.kt
@@ -5,13 +5,21 @@ import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import pt.ipt.dam2025.camerax.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_main)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        //   setContentView(R.layout.activity_main)
+
+        ViewCompat.setOnApplyWindowInsetsListener(binding.root  /*findViewById(R.id.main)*/) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
