f4540b90eaff769b422f48711daaef59475fd915
jcnpereira <casimiro@ipt.pt>
2025-10-31 19:38:42 +0000

    Change gradle values and dependencies

--- camerax/app/build.gradle.kts
+++ camerax/app/build.gradle.kts
@@ -35,9 +35,37 @@ android {
     kotlinOptions {
         jvmTarget = "11"
     }
+
+    // allow the access to objects of the interface, from code,
+    // in 'binding'
+    buildFeatures {
+        viewBinding = true
+    }
 }
 
 dependencies {
+
+    // CameraX core library using the camera2 implementation
+    val cameraxVersion = "1.5.1"
+    // The following line is optional, as the core library is included indirectly by camera-camera2
+    // implementation("androidx.camera:camera-core:${cameraxVersion}")
+    implementation(libs.androidx.camera.core)
+    // implementation("androidx.camera:camera-camera2:${cameraxVersion}")
+    implementation(libs.androidx.camera.camera2)
+    // If you want to additionally use the CameraX Lifecycle library
+    // implementation("androidx.camera:camera-lifecycle:${cameraxVersion}")
+    implementation(libs.androidx.camera.lifecycle)
+    // If you want to additionally use the CameraX VideoCapture library
+    //    implementation "androidx.camera:camera-video:${camerax_version}"
+    // If you want to additionally use the CameraX View class
+    // implementation("androidx.camera:camera-view:${cameraxVersion}")
+    implementation(libs.androidx.camera.view)
+    // If you want to additionally add CameraX ML Kit Vision Integration
+    //       implementation "androidx.camera:camera-mlkit-vision:${camerax_version}"
+    // If you want to additionally use the CameraX Extensions library
+    // implementation("androidx.camera:camera-extensions:${cameraxVersion}")
+    implementation(libs.androidx.camera.extensions)
+
     implementation(libs.androidx.core.ktx)
     implementation(libs.androidx.appcompat)
     implementation(libs.material)
